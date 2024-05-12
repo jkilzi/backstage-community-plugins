b19f815c871f4c80a84d594589ebb003a305da7a
Jonathan Kilzi <jkilzi@redhat.com>
2024-05-12 13:51:58 +0300

    adds cost-management-client
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

---
+++ workspaces/cost-management/plugins/cost-management-client/scripts/lib/file-reader.mjs
@@ -0,0 +1,36 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createInterface } from 'node:readline';
+
+
+export async function * createFileLineIterator(filePath) {
+    const fileStream = createReadStream(filePath);
+    const lineReader = createInterface({ input: fileStream });
+    let lineCounter = 0;
+    
+    for await (const line of lineReader) {
+        yield [line, lineCounter++];
+    }
+}
+
+export function parseLine(line, lineNumber, modelName) {
+    const patterns = [
+        [ new RegExp(`^export type ${modelName} = .+$`), 'type-alias' ],
+        [ new RegExp(`^export const ${modelName} = `), 'object-def' ],
+        [ new RegExp(`^\\s{2,4}(?<propName>(?!\\d)[\\w$]+): (?<propValue>['\\w-]+) as ${modelName},?`, 'i'), 'prop-def' ],
+        [ /^\};/, 'curly-brace-close']
+    ];
+
+    let result;
+    for (const [ pattern, kind ] of patterns) {
+        if (pattern.test(line)) {
+            result = [ pattern, kind ];
+            break;
+        }
+    }
+
+    if (!result) {
+        result = [ new Error(`Unknown syntax at line: ${lineNumber}`), null ];
+    }
+
+    return result;
+}
