feffb01014591f0a1c07b6cfd595c3a572b5b393
Jonathan Kilzi <jkilzi@redhat.com>
2024-06-13 17:32:45 +0300

    adds a sample app and backend for showcase purposes
    Adds also a PoC for the plugin icon

--- workspaces/resource-optimization/plugins/resource-optimization/dev/index.tsx
+++ workspaces/resource-optimization/plugins/resource-optimization/dev/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { createDevApp } from '@backstage/dev-utils';
 import { resourceOptimizationPlugin, ResourceOptimizationPage } from '../src/plugin';
+import { ResourceOptimizationIcon } from '../src/components/ResourceOptimizationIcon';
 
 createDevApp()
   .registerPlugin(resourceOptimizationPlugin)
   .addPage({
     element: <ResourceOptimizationPage />,
-    title: 'Root Page',
+    title: 'Optimizations',
+    icon: ResourceOptimizationIcon,
     path: '/resource-optimization',
+  })
+  .addPage({
+    element: (
+      <>
+        <h1>UNDER CONSTRUCTION</h1>
+        <h2>Here you'll find the Resource Optimization Details page</h2>
+      </>
+    ),
+    title: 'Optimization Details',
+    path: '/resource-optimization/:recommendationId',
   })
   .render();
