503d7c4f01fff01234145b55b342f73c3aae7014
Preeti Wadhwani <preeti.dev.git@gmail.com>
2024-06-18 16:59:45 +0530

    added UI for the first page of resource optimization plugin & fixed endpoint in config (#2)

--- workspaces/resource-optimization/plugins/resource-optimization/src/plugin.ts
+++ workspaces/resource-optimization/plugins/resource-optimization/src/plugin.ts
@@ -1,12 +1,37 @@
 import {
+  createApiFactory,
   createPlugin,
   createRoutableExtension,
+  discoveryApiRef,
+  fetchApiRef,
 } from '@backstage/core-plugin-api';
+import { Apis } from "@backstage-community/plugin-resource-optimization-common";
 
 import { rootRouteRef } from './routes';
+import { optimizationsApiRef } from './api/refs'
 
 export const resourceOptimizationPlugin = createPlugin({
   id: 'resource-optimization',
+  apis: [
+    createApiFactory({
+      api: optimizationsApiRef,
+      deps: {
+        discoveryApi: discoveryApiRef,
+        fetchApi: fetchApiRef,
+      },
+      factory({ discoveryApi, fetchApi }) {
+        return new Apis.OptimizationsApiClient({
+          discoveryApi: {
+            async getBaseUrl() {
+              const baseUrl = await discoveryApi.getBaseUrl('proxy');
+              return `${baseUrl}/cost-management/v1`;
+            },
+          },
+          fetchApi,
+        });
+      },
+    }),
+  ],
   routes: {
     root: rootRouteRef,
   },
