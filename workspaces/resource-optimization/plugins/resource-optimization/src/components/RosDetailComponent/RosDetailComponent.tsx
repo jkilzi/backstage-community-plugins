a291f6d03ee956fe1897101d755c350fe05d8686
Preeti Wadhwani <preeti.dev.git@gmail.com>
2024-06-27 14:19:32 +0530

    RHINENG-10603 landing page filters functionality (#4)
    * RHINENG-10603 added search filter component and filters functionality
    
    * passing id to detail page
    
    * reading id from route params

--- workspaces/resource-optimization/plugins/resource-optimization/src/components/RosDetailComponent/RosDetailComponent.tsx
+++ workspaces/resource-optimization/plugins/resource-optimization/src/components/RosDetailComponent/RosDetailComponent.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import {
-  Page,
-  Header
-} from '@backstage/core-components';
-
+import { Page, Header, Content } from '@backstage/core-components';
+import { Typography } from '@material-ui/core';
+import { useParams } from 'react-router-dom';
 
 export const RosDetailComponent = () => {
+  const { id } = useParams();
+
+  console.log('Checking id:', id);
 
   return (
     <Page themeId="tool">
-        <Header title="Resource Optimization Detail Page" />
+      <Header
+        title="Resource Optimization"
+        type="Optimizations"
+        typeLink="/resource-optimization"
+      />
+
+      <Content>
+        <Typography variant="h4">Container 1</Typography>
+      </Content>
     </Page>
   );
 };
