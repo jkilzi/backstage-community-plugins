dc3b9bfe2bfdf74a1c54f96f57a80b1746fc7184
Preeti Wadhwani <preeti.dev.git@gmail.com>
2024-07-02 19:02:56 +0530

    RHINENG-10787 detail page top section and recommendation section (#5)
    * RHINENG-10787 detail page top section and UI
    
    * removed memoryUnit & cpuUnit from query
    
    * code cleanup
    
    * handling multiple filter values
    
    * fixed cluster filter and detail page values
    
    * added util method to handle YAML code generation from API response

---
+++ workspaces/resource-optimization/plugins/resource-optimization/src/components/CodeInfoCard/CodeInfoCard.tsx
@@ -0,0 +1,48 @@
+import { CodeSnippet, InfoCard } from '@backstage/core-components';
+import { Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+  YAMLCodeDataType,
+  generateYAMLCode,
+} from '../../utils/generateYAMLCode';
+
+const sampleYAMLCodeData = {
+  limits: {
+    cpu: '-',
+    memory: '500Mi  # -85.1%',
+  },
+  requests: {
+    cpu: 0.1,
+    memory: '20Mi  # -86.1%',
+  },
+};
+
+interface CodeInfoCardProps {
+  cardTitle: string;
+  showCopyCodeButton: boolean;
+  yamlCodeData?: YAMLCodeDataType;
+}
+
+export const CodeInfoCard: React.FC<CodeInfoCardProps> = ({
+  cardTitle,
+  showCopyCodeButton,
+  yamlCodeData,
+}) => {
+  const YAMLCode = generateYAMLCode(yamlCodeData || sampleYAMLCodeData);
+
+  return (
+    <InfoCard
+      title={
+        <Typography variant="body1" style={{ fontWeight: 'bold' }}>
+          {cardTitle}
+        </Typography>
+      }
+    >
+      <CodeSnippet
+        text={YAMLCode}
+        language="yaml"
+        showCopyCodeButton={showCopyCodeButton}
+      />
+    </InfoCard>
+  );
+};
