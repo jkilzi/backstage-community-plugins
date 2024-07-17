ede106710d714b4c06bd8d32faf57e2af94ad062
Jonathan Kilzi <jkilzi@redhat.com>
2024-07-17 18:35:37 +0300

    adds deployment scripts
    adds export-dynamic top-level script

---
+++ workspaces/resource-optimization/scripts/00-test-code-quality.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "yarn install"
+yarn install
+
+echo "yarn tsc:full"
+yarn tsc:full
+
+echo "yarn lint"
+yarn lint
+
+echo "yarn prettier:check"
+yarn run -T prettier --check plugins/resource-optimization*
+
+
+echo "yarn test:all"
+yarn test:all
